ee31fc8fa92324e9769b5900452e1d657ea5c9b0
franciscofranco <franciscofranco.1990@gmail.com>
2014-06-30 23:42:38 +0000

    Update build.sh for zip creation after compilation. It's more for persona use but might help anyone build this by reading the code.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- build.sh
+++ build.sh
@@ -11,3 +11,14 @@ cp arch/arm/boot/zImage ../ramdisk_one_plus_one/split_img/boot.img-zImage
 cd ../ramdisk_one_plus_one/
 
 ./repackimg.sh
+
+cd ../one_plus_one/
+
+zipfile="franco.Kernel-nightly.zip"
+echo "making zip file"
+cp ../ramdisk_one_plus_one/image-new.img zip/boot.img
+cd zip/
+rm -f *.zip
+zip -r -9 $zipfile *
+rm -f /tmp/*.zip
+cp *.zip /tmp
