5e2751ed606887d98666ffaad1f82906aebf226d
franciscofranco <franciscofranco.1990@gmail.com>
2014-07-20 16:00:50 +0000

    board: kcal: add init values or the display boots completely black.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/oppo/board-8974-oppo.c
+++ arch/arm/mach-msm/oppo/board-8974-oppo.c
@@ -60,11 +60,10 @@
 #include "../../../drivers/video/msm/mdss/mdss_fb.h"
 extern int update_preset_lcdc_lut(void);
 
-int g_kcal_r;
-int g_kcal_g;
-int g_kcal_b;
-
-int g_kcal_min;
+int g_kcal_r = 255;
+int g_kcal_g = 255;
+int g_kcal_b = 255;
+int g_kcal_min = 35;
 
 int kcal_set_values(int kcal_r, int kcal_g, int kcal_b)
 {
