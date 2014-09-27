5217b89b68149fa9c7f814f84d7f6c0b8fbd3dbd
Riley Andrews <riandrews@google.com>
2014-09-26 17:36:36 -0700

    staging: binder: Change binder mutex to rtmutex.
    Surfaceflinger uses binder heavily to receive/send frames from applications
    while compositing the screen. Change the binder mutex to an rt mutex to minimize
    instances where high priority surfaceflinger binder work is blocked by lower
    priority binder ipc.
    
    Signed-off-by: Riley Andrews <riandrews@google.com>
    Change-Id: I086a715267648448f0c5f62b037a3093d1079a79
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -24,6 +24,7 @@
 #include <linux/miscdevice.h>
 #include <linux/mm.h>
 #include <linux/module.h>
+#include <linux/rtmutex.h>
 #include <linux/mutex.h>
 #include <linux/nsproxy.h>
 #include <linux/poll.h>
@@ -39,7 +40,7 @@
 #include "binder.h"
 #include "binder_trace.h"
 
-static DEFINE_MUTEX(binder_main_lock);
+static DEFINE_RT_MUTEX(binder_main_lock);
 static DEFINE_MUTEX(binder_deferred_lock);
 static DEFINE_MUTEX(binder_mmap_lock);
 
@@ -505,14 +506,14 @@ out_unlock:
 static inline void binder_lock(const char *tag)
 {
 	trace_binder_lock(tag);
-	mutex_lock(&binder_main_lock);
+	rt_mutex_lock(&binder_main_lock);
 	trace_binder_locked(tag);
 }
 
 static inline void binder_unlock(const char *tag)
 {
 	trace_binder_unlock(tag);
-	mutex_unlock(&binder_main_lock);
+	rt_mutex_unlock(&binder_main_lock);
 }
 
 static void binder_set_nice(long nice)
