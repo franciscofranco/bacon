dadfccf5d893300c805460f2785b5e3aef228c21
franciscofranco <franciscofranco.1990@gmail.com>
2014-09-29 16:29:50 +0000

    msm: thermal: fix init with msm_thermal
     * not using ioctl interface for this in-kernel solution
    so prevent msm_thermal-dev to be built
     * use higher frequency points than my N5 driver because
    Bacon rarely gets really hot so we don't need to throttle
    as heavy
     * re-structure init->register<->exit flow compared to the
    N5 code. We don't rely on board init call anymore. Besides
    externs are fugly. The rest is on the msm_thermal merge side
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/oppo/board-8974-oppo.c
+++ arch/arm/mach-msm/oppo/board-8974-oppo.c
@@ -196,7 +196,6 @@ void __init msm8974_add_drivers(void)
 	else
 		msm_clock_init(&msm8974_clock_init_data);
 	tsens_tm_init_driver();
-	msm_thermal_device_init();
 #ifdef CONFIG_LCD_KCAL
 	add_lcd_kcal_devices();
 #endif
