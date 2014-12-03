2313b5db1c67915fcd4d4bf59b0547b4c899f781
franciscofranco <franciscofranco.1990@gmail.com>
2014-12-03 03:43:08 +0000

    prima: revert to stock CM drivers
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/staging/prima/CORE/VOSS/inc/wlan_hdd_misc.h
+++ drivers/staging/prima/CORE/VOSS/inc/wlan_hdd_misc.h
@@ -44,10 +44,10 @@
 
 #ifdef MSM_PLATFORM
 #ifdef QC_WLAN_CHIPSET_PRIMA
-#define WLAN_INI_FILE              "../../../sbin/wlan/WCNSS_qcom_cfg.ini"
-#define WLAN_CFG_FILE              "../../../sbin/wlan/WCNSS_cfg.dat"
+#define WLAN_INI_FILE              "wlan/prima/WCNSS_qcom_cfg.ini"
+#define WLAN_CFG_FILE              "wlan/prima/WCNSS_cfg.dat"
 #define WLAN_FW_FILE               ""
-#define WLAN_NV_FILE               "../../../sbin/wlan/WCNSS_qcom_wlan_nv.bin"
+#define WLAN_NV_FILE               "wlan/prima/WCNSS_qcom_wlan_nv.bin"
 #define WLAN_DICT_FILE             "wlan/prima/WCNSS_qcom_wlan_dictionary.dat"
 #define WLAN_COUNTRY_INFO_FILE     "wlan/prima/WCNSS_wlan_country_info.dat"
 #define WLAN_HO_CFG_FILE           "wlan/prima/WCNSS_wlan_ho_config"
