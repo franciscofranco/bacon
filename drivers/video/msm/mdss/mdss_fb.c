1fa7b401162680baf6ac8c3e733be1e17e59fcaf
franciscofranco <franciscofranco.1990@gmail.com>
2016-02-27 07:30:23 +0000

    mdss: set fb0 thread to interruptible to lower load_average
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2654,9 +2654,12 @@ static int __mdss_fb_display_thread(void *data)
 				mfd->index);
 
 	while (1) {
-		wait_event(mfd->commit_wait_q,
+		ret = wait_event_interruptible(mfd->commit_wait_q,
 				(atomic_read(&mfd->commits_pending) ||
 				 kthread_should_stop()));
+
+		if (ret)
+			continue;
 
 		if (kthread_should_stop())
 			break;
